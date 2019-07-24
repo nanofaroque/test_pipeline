d3e73a7cbe68b9f726360a58ddd3ec9fbb7fc7c0
The Omar <faroque.ae@gmail.com>
2019-07-24 14:36:38 -0400

    hello test

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-def file_write(input_str):
+import json
+def file_write():
     file = open('testfile.txt', 'a')
-
     person_dict = {'name': 'Bob',
                     'age': 12,
                     'children': None
                     }
-                    person_json = json.dumps(person_dict)
-    file.write(person_dict)
+    person_json = json.dumps(person_dict)
+    file.write(''.join(str(person_dict)))
+
+file_write()
