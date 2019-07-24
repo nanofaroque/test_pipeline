0f536297aa082a0f9c241d351cecfeaf46b7b8de
The Omar <faroque.ae@gmail.com>
2019-07-24 17:47:58 -0400

    minor cleaning

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 import json
+
+
 def file_write_omar(data):
     file = open('testfile.json', 'a')
-    #person_json = json.dumps(data)
     file.write(''.join(str(data)))
 
+
 omar_dict = {'name': 'Omar',
-            'age': 33,
-            'children': 'Eshika'
-                }
+             'age': 33,
+             'children': 'Eshika'
+             }
 file_write_omar(omar_dict)
+
+
+def test():
+    with open('sample.xml', 'r') as f, open('new_sample.xml', 'w') as g:
+        g.write('<dependencies>{}</dependencies>'.format(f.read()))
+
+        import xml.etree.ElementTree as ET
+        tree = ET.parse('new_sample.xml')
+        root = tree.getroot()
+        # print(root)
